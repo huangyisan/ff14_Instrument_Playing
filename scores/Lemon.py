13849a9ba809c8c4da8fc730d392d756b1aeada4
huangyisan <anonymousyisan@gmail.com>
2020-11-01 03:17:06 +0800

    lemon update

--- scores/Lemon.py
+++ scores/Lemon.py
@@ -17,91 +17,115 @@ four_time = 4 / base_time
 
 if __name__ == '__main__':
 
-    time.sleep(two_time)
-    # Line 1
-    Pause(one_time)
-    Pause(one_time)
-    Pause(one_time)
-    Treble_Do(half_time)
-    Treble_Re(half_time)
-    #
-    Treble_Mi(one_time)
-    Treble_Do(half_time)
-    Normal_La(one_half_time)
-    Treble_Re(one_time)
-    #
-    Normal_Si(one_time)
-    Normal_So(half_time)
-    Normal_Mi(one_half_time)
-    Normal_Si(one_time)
-    #
-    Normal_La(one_time)
-    Normal_So(half_time)
-    Normal_Do(one_half_time)
-    Normal_So(one_time)
-    #
-    Normal_Mi(three_time)
-    Normal_Re(half_time)
-    Normal_Mi(half_time)
+    time.sleep(2)
+    def repeat():
+        # Line 1
+        Pause(one_time)
+        Pause(one_time)
+        Pause(one_time)
+        Treble_Do(half_time)
+        Treble_Re(half_time)
+        #
+        Treble_Mi(one_time)
+        Treble_Do(half_time)
+        Normal_La(one_half_time)
+        Treble_Re(one_time)
+        #
+        Normal_Si(one_time)
+        Normal_So(half_time)
+        Normal_Mi(one_half_time)
+        Normal_Si(one_time)
+        #
+        Normal_La(one_time)
+        Normal_So(half_time)
+        Normal_Do(one_half_time)
+        Normal_So(one_time)
+        #
+        Normal_Mi(three_time)
+        Normal_Re(half_time)
+        Normal_Mi(half_time)
 
-    # Line 2
-    Normal_Fa(two_time)
+        # Line 2
+        Normal_Fa(two_time)
+        Treble_Do(one_time)
+        Normal_Si(one_time)
+        Treble_Do(half_time)
+        #
+        Normal_So(two_time)
+        Normal_Fa(one_time)
+        Normal_Mi(half_quarter_time)
+        Normal_Fa(quarter_time)
+        #
+        Normal_Fa_s(two_time)
+        Treble_Do(one_time)
+        Normal_Si(one_time)
+        Normal_La(half_time)
+        Normal_So_s(three_time)
+        Treble_Do(half_time)
+        Treble_Re(half_time)
+        #
+        Treble_Mi(one_time)
+        Treble_Do(half_time)
+        Normal_La(one_half_time)
+        Treble_Re(one_time)
+
+        # Line 3
+        Normal_Si(one_time)
+        Normal_So(half_time)
+        Normal_Mi(one_half_time)
+        Normal_Si(one_time)
+        #
+        Normal_La(one_time)
+        Normal_So(half_time)
+        Normal_Do(one_half_time)
+        Normal_So(one_time)
+        #
+        Normal_Mi(three_time)
+        Normal_Re(half_time)
+        Normal_Mi(half_time)
+        #
+        Normal_Fa(two_time)
+        Normal_So(one_time)
+        Normal_Fa(one_time)
+        Normal_So(half_time)
+        #
+        Normal_Mi(one_time)
+        Normal_So(one_time)
+        Treble_Do(one_time)
+        Treble_Mi(one_time)
+        #
+        Treble_Re(half_time)
+        Treble_Re(one_half_time)
+        Treble_Re(half_time)
+        Treble_Do(one_half_time)
+        #
+        Treble_Do(four_time)
+
+    repeat()
+    Pause(four_time)
+    # repeat
+    repeat()
+
+    # Line 8
+    Normal_La(one_half_time)
+    Normal_Si(half_time)
     Treble_Do(one_time)
-    Normal_Si(one_time)
-    Treble_Do(half_time)
-    #
-    Normal_So(two_time)
-    Normal_Fa(one_time)
-    Normal_Mi(half_quarter_time)
-    Normal_Fa(quarter_time)
-    #
-    Normal_Fa_s(two_time)
-    Treble_Do(one_time)
-    Normal_Si(one_time)
+    Normal_Si(half_time)
     Normal_La(half_time)
-    Normal_So_s(three_time)
-    Treble_Do(half_time)
-    Treble_Re(half_time)
     #
+    Normal_So(one_time)
     Treble_Mi(one_time)
-    Treble_Do(half_time)
-    Normal_La(one_half_time)
-    Treble_Re(one_time)
-
-    # Line 3
-    Normal_Si(one_time)
-    Normal_So(half_time)
-    Normal_Mi(one_half_time)
-    Normal_Si(one_time)
-    #
-    Normal_La(one_time)
-    Normal_So(half_time)
-    Normal_Do(one_half_time)
-    Normal_So(one_time)
-    #
-    Normal_Mi(three_time)
-    Normal_Re(half_time)
-    Normal_Mi(half_time)
-    #
-    Normal_Fa(two_time)
-    Normal_So(one_time)
-    Normal_Fa(one_time)
-    Normal_So(half_time)
-    #
-    Normal_Mi(one_time)
-    Normal_So(one_time)
-    Treble_Do(one_time)
-    Treble_Mi(one_time)
+    Treble_Mi(two_time)
     #
     Treble_Re(one_half_time)
+    Treble_Mi(half_time)
+    Treble_Fa(one_time)
+    Treble_Mi(half_time)
     Treble_Re(half_time)
-    Treble_Re(half_time)
-    Treble_Do(one_time)
-    Treble_Do(half_time)
     #
-    Treble_Do(four_time)
-
-
+    Treble_Do(one_time)
+    Treble_Re(one_time)
+    Normal_So(two_time)
 
 
 
