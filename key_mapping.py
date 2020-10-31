e3b62977d5e10974e56a6188e236f2c0f35dd68e
huangyisan <anonymousyisan@gmail.com>
2020-11-01 01:49:51 +0800

    英雄的黎明-圆号

--- key_mapping.py
+++ key_mapping.py
@@ -1,29 +1,98 @@
 import pyautogui
 
+shift = 't'
+ctrl = 'b'
 # Bass
-B_Do = ['b','q']
-B_Re = ['b','w']
-B_Mi = ['b','e']
-B_Fa = ['b','r']
-B_So = ['b','t']
-B_La = ['b','y']
-B_Si = ['b','u']
+_note_Bass_Do = [ctrl,'q']
+_note_Bass_Re = [ctrl,'w']
+_note_Bass_Mi = [ctrl,'e']
+_note_Bass_Fa = [ctrl,'r']
+_note_Bass_So = [ctrl,'t']
+_note_Bass_La = [ctrl,'y']
+_note_Bass_Si = [ctrl,'u']
 
 # Normal
-Do = ['n','q']
-Re = ['n','w']
-Mi = ['n','e']
-Fa = ['n','r']
-So = ['n','t']
-La = ['n','y']
-Si = ['n','u']
+_note_Normal_Do = ['n','q']
+_note_Normal_Re = ['n','w']
+_note_Normal_Mi = ['n','e']
+_note_Normal_Fa = ['n','r']
+_note_Normal_So = ['n','t']
+_note_Normal_La = ['n','y']
+_note_Normal_Si = ['n','u']
 
 # Treble
-T_Do = ['t','q']
-T_Re = ['t','w']
-T_Mi = ['t','e']
-T_Fa = ['t','r']
-T_So = ['t','t']
-T_La = ['t','y']
-T_Si = ['t','u']
+_note_Treble_Do = [shift,'q']
+_note_Treble_Re = [shift,'w']
+_note_Treble_Mi = [shift,'e']
+_note_Treble_Fa = [shift,'r']
+_note_Treble_So = [shift,'t']
+_note_Treble_La = [shift,'y']
+_note_Treble_Si = [shift,'u']
+
+def press_a_key(note, ptime):
+    if note[0] == ctrl:
+        pyautogui.keyDown('ctrl')
+        pyautogui.keyDown(note[1],pause=ptime)
+        pyautogui.keyUp(note[1])
+        pyautogui.keyUp('ctrl')
+    elif note[0] == 'n':
+        pyautogui.keyDown(note[1],pause=ptime)
+        pyautogui.keyUp(note[1])
+    elif note[0] == shift:
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown(note[1],pause=ptime)
+        pyautogui.keyUp(note[1])
+        pyautogui.keyUp('shift')
+
+
+def Bass_Do(ptime):
+    press_a_key(_note_Bass_Do, ptime)
+def Bass_Re(ptime):
+    press_a_key(_note_Bass_Re, ptime)
+def Bass_Mi(ptime):
+    press_a_key(_note_Bass_Mi, ptime)
+def Bass_Fa(ptime):
+    press_a_key(_note_Bass_Fa, ptime)
+def Bass_So(ptime):
+    press_a_key(_note_Bass_So, ptime)
+def Bass_La(ptime):
+    press_a_key(_note_Bass_La, ptime)
+def Bass_Si(ptime):
+    press_a_key(_note_Bass_Si, ptime)
+
+
+def Normal_Do(ptime):
+    press_a_key(_note_Normal_Do, ptime)
+def Normal_Re(ptime):
+    press_a_key(_note_Normal_Re, ptime)
+def Normal_Mi(ptime):
+    press_a_key(_note_Normal_Mi, ptime)
+def Normal_Fa(ptime):
+    press_a_key(_note_Normal_Fa, ptime)
+def Normal_So(ptime):
+    press_a_key(_note_Normal_So, ptime)
+def Normal_La(ptime):
+    press_a_key(_note_Normal_La, ptime)
+def Normal_Si(ptime):
+    press_a_key(_note_Normal_Si, ptime)
+
+
+def Treble_Do(ptime):
+    press_a_key(_note_Treble_Do, ptime)
+def Treble_Re(ptime):
+    press_a_key(_note_Treble_Re, ptime)
+def Treble_Mi(ptime):
+    press_a_key(_note_Treble_Mi, ptime)
+def Treble_Fa(ptime):
+    press_a_key(_note_Treble_Fa, ptime)
+def Treble_So(ptime):
+    press_a_key(_note_Treble_So, ptime)
+def Treble_La(ptime):
+    press_a_key(_note_Treble_La, ptime)
+def Treble_Si(ptime):
+    press_a_key(_note_Treble_Si, ptime)
+
+
+
+
 
---
+++ scores/英雄的黎明.py
@@ -0,0 +1,68 @@
+from key_mapping import *
+import time
+# 4/4
+
+base_time = 2
+
+half_time = 0.5 / base_time
+one_time = 1 / base_time
+two_time = 2 / base_time
+three_time = 3 / base_time
+
+if __name__ == '__main__':
+
+    time.sleep(two_time)
+    #
+    Bass_La(one_time)
+    Normal_Mi(two_time)
+    Normal_Re(one_time)
+    #
+    Normal_Do(half_time)
+    Bass_La(half_time)
+    Bass_La(two_time)
+    Bass_La(half_time)
+    Bass_Si(half_time)
+    #
+    Normal_Do(one_time)
+    Normal_Do(half_time)
+    Normal_Re(half_time)
+    Normal_Mi(one_time)
+    Normal_So(one_time)
+    #
+    Normal_Mi(half_time)
+    Normal_Re(half_time)
+    Bass_La(three_time)
+    #
+    Bass_So(one_time)
+    Normal_Re(two_time)
+    Bass_So(one_time)
+    #
+    Normal_Do(half_time)
+    Bass_La(half_time)
+    Bass_La(three_time)
+    #
+    Bass_So(one_time)
+    Normal_Re(two_time)
+    Bass_So(one_time)
+    #
+    Normal_Do(half_time)
+    Bass_La(half_time)
+    Bass_La(three_time)
+    #
+    Bass_La(one_time)
+    Normal_Mi(two_time)
+    Normal_Re(one_time)
+    #
+    Normal_Do(half_time)
+    Bass_La(half_time)
+    Bass_La(two_time)
+    Bass_La(half_time)
+    Bass_Si(half_time)
+    #
+    Normal_Do(one_time)
+    Normal_Do(half_time)
+    Normal_Re(half_time)
+    Normal_Mi(one_time)
+    Normal_So(one_time)
+
+
