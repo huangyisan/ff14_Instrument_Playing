d72aa4a9b7f62d4244f20e78354662451a08174d
huangyisan <anonymousyisan@gmail.com>
2020-11-01 00:43:05 +0800

    update

---
+++ key_mapping.py
@@ -0,0 +1,29 @@
+import pyautogui
+
+# Bass
+B_Do = ['b','q']
+B_Re = ['b','w']
+B_Mi = ['b','e']
+B_Fa = ['b','r']
+B_So = ['b','t']
+B_La = ['b','y']
+B_Si = ['b','u']
+
+# Normal
+Do = ['n','q']
+Re = ['n','w']
+Mi = ['n','e']
+Fa = ['n','r']
+So = ['n','t']
+La = ['n','y']
+Si = ['n','u']
+
+# Treble
+T_Do = ['t','q']
+T_Re = ['t','w']
+T_Mi = ['t','e']
+T_Fa = ['t','r']
+T_So = ['t','t']
+T_La = ['t','y']
+T_Si = ['t','u']
+
